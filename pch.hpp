035202cf74a7d939c0547d17a4db619350fe4a8d
dragon-archer <dragon-archer@outlook.com>
2023-07-20 23:14:00 +0800

    feat(base): Add CMake and PCH

---
+++ pch.hpp
@@ -0,0 +1,26 @@
+/* SPDX-License-Identifier: MIT */
+/**
+ * @file      pch.hpp
+ * @brief     Pre-compile header for script
+ * @version   0.1
+ * @author    dragon-archer
+ *
+ * @copyright Copyright (c) 2023 dragon-archer
+ */
+
+#ifndef _DA_SCRIPT_PCH_HPP_
+#define _DA_SCRIPT_PCH_HPP_
+
+#include <da/config.hpp>
+#include <da/hash.hpp>
+#include <da/preprocessor/enum.hpp>
+#include <spdlog/sinks/rotating_file_sink.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/spdlog.h>
+#include <cstring>
+#include <memory>
+#include <source_location>
+#include <string_view>
+#include <vector>
+
+#endif // _DA_SCRIPT_PCH_HPP_
