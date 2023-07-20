b78ec878179554d088c62752e5e7da87312a7a7e
dragon-archer <dragon-archer@outlook.com>
2023-07-20 23:41:07 +0800

    feat(base): Add entry point

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* SPDX-License-Identifier: MIT */
+/**
+ * @file      main.cpp
+ * @brief     Entry point for dascript
+ * @version   0.1
+ * @author    dragon-archer
+ *
+ * @copyright Copyright (c) 2023 dragon-archer
+ */
+
+#include "config.hpp"
+#include "log.hpp"
+using namespace DA_SCRIPT;
+
+int main() {
+	log_info("DaScript Start");
+	log_info("DaScript Exit");
+	return 0;
+}
