f3d06b1b151e8e5e8407793c889601cd5d1912c8
dragon-archer <dragon-archer@outlook.com>
2023-07-24 14:51:35 +0800

    feat(function): Add global function and mock for it

---
+++ global.hpp
@@ -0,0 +1,56 @@
+/* SPDX-License-Identifier: MIT */
+/**
+ * @file      global.hpp
+ * @brief     Global objects
+ * @version   0.1
+ * @author    dragon-archer
+ *
+ * @copyright Copyright (c) 2023 dragon-archer
+ */
+
+#ifndef _DA_SCRIPT_GLOBAL_HPP_
+#define _DA_SCRIPT_GLOBAL_HPP_
+
+#include "config.hpp"
+#include "log.hpp"
+#include "object.hpp"
+
+DA_BEGIN_SCRIPT
+
+inline auto& get_global_object_store() noexcept {
+	static std::unordered_map<size_t, object_ptr> instance;
+	return instance;
+}
+
+inline object_ptr get_global_object(size_t id) {
+	auto it = get_global_object_store().find(id);
+	if(it == get_global_object_store().end()) {
+		return nullptr;
+	} else {
+		return it->second;
+	}
+}
+
+inline size_t random_id() noexcept {
+	static std::mt19937_64 engine(std::time(nullptr));
+	return engine();
+}
+
+template<typename T, typename... Args>
+inline auto create_global_object_with_id(size_t id, Args&&... args) {
+	auto ptr          = std::make_shared<T>(std::forward<Args>(args)...);
+	auto [_, success] = get_global_object_store().try_emplace(id, ptr);
+	DA_IFUNLIKELY(!success) {
+		log_error("Failed create global object, id: {}", id);
+	}
+	return std::pair{ptr, id};
+}
+
+template<typename T, typename... Args>
+inline auto create_global_object(Args&&... args) {
+	return create_global_object_with_id(random_id(), std::forward<Args>(args)...);
+}
+
+DA_END_SCRIPT
+
+#endif // _DA_SCRIPT_GLOBAL_HPP_
--- main.cpp
+++ main.cpp
@@ -9,11 +9,32 @@
  */
 
 #include "config.hpp"
+#include "init.hpp"
 #include "log.hpp"
 using namespace DA_SCRIPT;
 
+void mock() {
+	/*
+		Mock for:
+		value = {
+			add = 5
+			add = 6
+		}
+	*/
+	auto v = std::make_shared<compound_number>();
+	v->get_compound().emplace_back(std::make_shared<statement>(
+		get_global_object(da::hash("add")),
+		std::make_shared<plain_number>(5)));
+	v->get_compound().emplace_back(std::make_shared<statement>(
+		get_global_object(da::hash("add")),
+		std::make_shared<plain_number>(6)));
+	log_debug("{}", v->get_value());
+}
+
 int main() {
-	log_info("DaScript Start");
-	log_info("DaScript Exit");
+	log_info("dascript start");
+	init_plain_functions();
+	mock();
+	log_info("dascript exit");
 	return 0;
 }
