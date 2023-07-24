0eeb82f6a9ccabf56c97ada831b0bfe4c36d9a97
dragon-archer <dragon-archer@outlook.com>
2023-07-24 14:50:14 +0800

    feat(function): Add plain_function

--- function.hpp
+++ function.hpp
@@ -29,6 +29,30 @@ class function : virtual public object {
 	virtual void operator()(scope_ptr scope_, object_ptr param, object_ptr ret) = 0;
 };
 
+using plain_function_impl = std::function<void(scope_ptr, object_ptr, object_ptr)>;
+
+class plain_function : public function {
+	plain_function_impl _func{};
+
+	public:
+	plain_function() noexcept = default;
+	plain_function(const plain_function_impl& func)
+		: _func(func) { }
+	virtual ~plain_function() = default;
+
+	virtual constexpr types type() const noexcept override {
+		return types::plain_function;
+	}
+
+	constexpr auto& get() noexcept {
+		return _func;
+	}
+
+	virtual void operator()(scope_ptr s, object_ptr p, object_ptr r) override {
+		_func(s, p, r);
+	}
+};
+
 DA_END_SCRIPT
 
 #endif // _DA_SCRIPT_FUNCTION_HPP_
