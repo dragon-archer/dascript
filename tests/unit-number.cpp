6417aee68f3a6bcbfaaf33d7cf40f3ad569c9577
dragon-archer <dragon-archer@outlook.com>
2023-07-26 13:03:51 +0800

    test(number): Add more number tests

--- tests/unit-number.cpp
+++ tests/unit-number.cpp
@@ -14,11 +14,53 @@
 TEST_CASE("number") {
 	using namespace DA_SCRIPT;
 
+	SUBCASE("value") {
+		auto v = std::make_shared<compound_number>();
+		v->get_compound().emplace_back(std::make_shared<statement>(
+			get_global_object(da::hash("value")),
+			std::make_shared<plain_number>(4)));
+		CHECK_EQ(v->get_value(), 4);
+	}
+
 	SUBCASE("add") {
 		auto v = std::make_shared<compound_number>();
 		v->get_compound().emplace_back(std::make_shared<statement>(
 			get_global_object(da::hash("add")),
 			std::make_shared<plain_number>(5)));
 		CHECK_EQ(v->get_value(), 5);
+	}
+
+	SUBCASE("subtract") {
+		auto v = std::make_shared<compound_number>();
+		v->get_compound().emplace_back(std::make_shared<statement>(
+			get_global_object(da::hash("subtract")),
+			std::make_shared<plain_number>(6)));
+		CHECK_EQ(v->get_value(), -6);
+	}
+
+	SUBCASE("mix") {
+		auto v = std::make_shared<compound_number>();
+		v->get_compound().emplace_back(std::make_shared<statement>(
+			get_global_object(da::hash("value")),
+			std::make_shared<plain_number>(1)));
+		v->get_compound().emplace_back(std::make_shared<statement>(
+			get_global_object(da::hash("add")),
+			std::make_shared<plain_number>(2)));
+		v->get_compound().emplace_back(std::make_shared<statement>(
+			get_global_object(da::hash("subtract")),
+			std::make_shared<plain_number>(4)));
+		v->get_compound().emplace_back(std::make_shared<statement>(
+			get_global_object(da::hash("max")),
+			std::make_shared<plain_number>(-2)));
+		v->get_compound().emplace_back(std::make_shared<statement>(
+			get_global_object(da::hash("multiply")),
+			std::make_shared<plain_number>(5)));
+		v->get_compound().emplace_back(std::make_shared<statement>(
+			get_global_object(da::hash("min")),
+			std::make_shared<plain_number>(-8)));
+		v->get_compound().emplace_back(std::make_shared<statement>(
+			get_global_object(da::hash("divide")),
+			std::make_shared<plain_number>(3)));
+		CHECK_EQ(v->get_value(), -2);
 	}
 }
